6f82c0771503d96e68fb40cd936fd2783c3817f6
Zach Adams <zornslemons@proton.me>
2026-02-12 19:49:11 -0700

    WIP 003

--- 003/003.go
+++ 003/003.go
@@ -1,11 +1,22 @@
 // The prime factors of 13195 are 5, 7, 13 and 29
 // What is the largest prime factor of the number 600851475143?
-// Solution: Prime facotrization is at the center of RSA for a damn REASON. Not a lot to do here except do it the hard way. 
-// We could do this by reading from a text file that we populate with prime numbers up to n/2 but like...that feels like cheating and 
-// not really what I want ot do even though it's easier. Instead, I'm going to just the test division method and see if I can come up with 
-// any cute ways to cut down the search space as we go. 
+// Solution: Prime facotrization is at the center of RSA for a damn REASON. Not a lot to do here except do it the hard way.
+// We could do this by reading from a text file that we populate with prime numbers up to n/2 but like...that feels like cheating and
+// not really what I want ot do even though it's easier. Instead, I'm going to just the test division method and see if I can come up with
+// any cute ways to cut down the search space as we go.
 //
+// We're going use a binary tree data structure.
 package main
 
+func main() {
+}
 
+// this is our factor tree data structure.
+type Node struct {
+	value uint
+	left  *Node
+	right *Node
+}
 
+func factor(number uint) (uint, uint) {
+}
